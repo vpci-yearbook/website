857db5f87985a1238380ed83dc70edf91f06f287
Alec Li <yunshen08@gmail.com>
2024-08-16 16:20:24 -0400

    codebase setup

---
+++ frontend/app/page.js
@@ -0,0 +1,14 @@
+import Hero from '@/components/landing/Hero'
+import Nav from '@/components/Nav'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <Nav />
+      <Hero />
+    </>
+  )
+}
+
+export default Home
