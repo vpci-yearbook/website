8848d8625246fea047f6b7dc35460c63828bc640
Alec Li <yunshen08@gmail.com>
2024-08-17 00:30:32 -0400

    gallery

--- frontend/app/gallery/page.jsx
+++ frontend/app/gallery/page.jsx
@@ -1,8 +1,19 @@
+import Filter from '@/components/gallery/Filter'
+import Photos from '@/components/gallery/Gallery'
+import Header from '@/components/gallery/Header'
+import Nav from '@/components/Nav'
 import React from 'react'
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <div>
+      <Nav />
+      <Header />
+      <div className='max-w-screen-xl mx-auto grid grid-cols-2'>
+        <Filter />
+        <Photos />
+      </div>
+    </div>
   )
 }
 
