5668706c27bdb89d79799b27e2030c790f658438
Alec Li <yunshen08@gmail.com>
2024-08-26 14:28:34 -0400

    timestamps

--- frontend/components/gallery/Gallery.jsx
+++ frontend/components/gallery/Gallery.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Photos() {
-  const [images, setImages] = useState([]);
+  const [groupedImages, setGroupedImages] = useState([]);
   const [containerWidth, setContainerWidth] = useState(0);
   const containerRef = useRef(null);
   const ngrokUrl = 'https://0a9b-162-221-127-80.ngrok-free.app';
@@ -16,7 +16,7 @@ export default function Photos() {
       try {
         const response = await fetch('/api/images/previews');
         const data = await response.json();
-        setImages(data);
+        setGroupedImages(data);
       } catch (error) {
         console.error('Error fetching image previews:', error);
       }
@@ -40,13 +40,12 @@ export default function Photos() {
     };
   }, []);
 
-  const calculateRows = () => {
+  const calculateRows = (images) => {
     const rows = [];
     let currentRow = [];
     let currentRowWidth = 0;
 
     images.forEach((image) => {
-      console.log('image', image);
       const aspectRatio = image['preview_width'] / image['preview_height'];
       const adjustedWidth = aspectRatio * targetRowHeight;
 
@@ -70,26 +69,28 @@ export default function Photos() {
     return rows;
   };
 
-  const rows = containerWidth ? calculateRows() : [];
-
-  console.log('containerWidth', containerWidth);
-  console.log('rows', rows);
+  console.log('groupedImages', groupedImages);
 
   return (
     <div className="w-full mx-auto" ref={containerRef}>
-      {rows.map((row, rowIndex) => (
-        <div key={rowIndex} className="flex flex-row mb-4">
-          {row.images.map((image) => (
-            <div
-              key={image.file_id}
-              className="p-2 cursor-pointer"
-              onClick={() => router.push(`/gallery/${image.file_id}`)}
-            >
-              <img
-                src={`${ngrokUrl}/images/${image.file_id}/preview`}
-                alt={image.filename}
-                style={{ height: row.height, width: 'auto' }}
-              />
+      {groupedImages.map(([date, images]) => (
+        <div key={date} className="mb-6">
+          <h2 className="text-lg font-semibold mb-2">{date}</h2>
+          {calculateRows(images).map((row, rowIndex) => (
+            <div key={rowIndex} className="flex flex-row mb-4">
+              {row.images.map((image) => (
+                <div
+                  key={image.file_id}
+                  className="p-2 cursor-pointer"
+                  onClick={() => router.push(`/gallery/${image.file_id}`)}
+                >
+                  <img
+                    src={`${ngrokUrl}/images/${image.file_id}/preview`}
+                    alt={image.filename}
+                    style={{ height: row.height, width: 'auto' }}
+                  />
+                </div>
+              ))}
             </div>
           ))}
         </div>
